58245d29a30b4a59587ca3f07b95c91ecc1e2dd3
Ethan Cleminson <128926337+Etmannn@users.noreply.github.com>
2025-01-07 10:49:32 +0000

    Update to application commands and I fixed some of the comments

--- Bot.py
+++ Bot.py
@@ -1,6 +1,6 @@
 # Terraria Discord Bot by Ethan Cleminson | 22/12/2024
-# Packages used in program
 
+# Packages used in program
 import discord
 from discord.ext import commands
 import subprocess
@@ -14,8 +14,9 @@ with open('config.yaml', 'r') as f:
 # Set discord bot intents
 intents = discord.Intents.default()
 intents.message_content = True
-bot = commands.Bot(command_prefix=config['prefix'], intents=intents)
+bot = commands.Bot(command_prefix='/', intents=intents)
 bot.remove_command("help")
+
 
 # Pings server to check if its online
 def serverping():
@@ -24,40 +25,41 @@ def serverping():
     flag = 0
     for process in f.Win32_Process():
         if "TerrariaServer.exe" == process.Name:
-            return True
             flag = 1
-            break
+            return True
 
     if flag == 0:
         return False
+
 
 # Prints a message and sets activitiy to the help command; triggers when the bot is online
 @bot.event
 async def on_ready():
     print("Bot successfully logged in")
-    await bot.change_presence(
-        activity=discord.Activity(type=discord.ActivityType.listening, name=f"{config['prefix']}help")
-    )
+    await bot.change_presence(activity=discord.Activity(
+        type=discord.ActivityType.listening, name=f"{config['prefix']}help"))
+
 
 # A command to check if the server is online
-@bot.command()
-async def ping(ctx):
+@bot.tree.command(name="ping", description="Checks if the server is online")
+async def ping(interaction: discord.Interaction):
     online = serverping()
     if online is True:
-        await ctx.send("Server is Online!")
+        await interaction.response.send_message("** Server is Online! **")
     if online is False:
-        await ctx.send("Server is Offline")
+        await interaction.response.send_message("** Server is Offline **")
+
 
 # A command to start the Terraria server
 # If the server is already online it will inform the user
-@bot.command()
-async def start(ctx):
+@bot.tree.command(name="start", description="Starts the Terraria server")
+async def start(interaction: discord.Interaction):
     online = serverping()
     if online is True:
-        await ctx.send("Server already Online")
+        await interaction.response.send_message("Server already Online")
     else:
-        await ctx.send("Server is starting!")
-        subprocess.Popen(f"start cmd.exe /c {config['serverdir']}", shell=True)
+        await interaction.response.send_message("Server is starting!")
+        subprocess.Popen(f"start cmd.exe /c {config['startdir']}", shell=True)
         if config['playit'] == 'true':
             subprocess.call("TASKKILL /F /IM playit.exe")
             subprocess.Popen('start cmd.exe /c playit.exe', shell=True)
@@ -65,48 +67,57 @@ async def start(ctx):
         while checking is True:
             checkstat = serverping()
             if checkstat is True:
-                await ctx.send("Server Online!")
+                await interaction.followup.send("Server Online!")
                 checking = False
+
+
 # A command to stop the Terraria server
-#
-@bot.command()
-async def stop(ctx):
+@bot.tree.command(name="stop", description="Stops the Terraria server")
+async def stop(interaction: discord.Interaction):
     online = serverping()
     if online is True:
         subprocess.call("TASKKILL /F /IM TerrariaServer.exe")
         if config['playit'] == 'true':
             subprocess.call("TASKKILL /F /IM playit.exe")
-        await ctx.send("Server Offline")
+        await interaction.response.send_message("Server Offline")
     else:
-        await ctx.send("Sever already Offline")
+        await interaction.response.send_message("Sever already Offline")
+
 
 # A command to restart the Terraria server
-@bot.command()
-async def restart(ctx):
+@bot.tree.command(name="restart", description="Restarts the Terraria server")
+async def restart(interaction: discord.Interaction):
     online = serverping()
     if online is True:
         await stop(ctx)
         await start(ctx)
     else:
-        await ctx.send("Server Offline")
+        await interaction.response.send_message("Server Offline")
 
 
 # Displays all the commands of the bot in a discord embed
-@bot.command()
-async def help(ctx):
+@bot.tree.command(name="help", description="Lists all avaible commands")
+async def help(interaction: discord.Interaction):
     helpembed = discord.Embed(title="Commands", color=0x55FF55)
 
-    helpembed.add_field(name=f"{config['prefix']}ping", value="Checks Server Status", inline=False)
-    helpembed.add_field(
-        name=f"{config['prefix']}start", value="Starts the Terraria server", inline=False
-    )
-    helpembed.add_field(
-        name=f"{config['prefix']}stop", value="Stops the Terraria server", inline=False
-    )
-    helpembed.add_field(
-        name=f"{config['prefix']}restart", value="Restarts the Terraria server", inline=False
-    )
-    await ctx.send(embed=helpembed)
+    helpembed.add_field(name=f"{config['prefix']}ping",
+                        value="Returns whether or not the server is online",
+                        inline=False)
+    helpembed.add_field(name=f"{config['prefix']}start",
+                        value="Starts the Terraria server",
+                        inline=False)
+    helpembed.add_field(name=f"{config['prefix']}stop",
+                        value="Stops the Terraria server",
+                        inline=False)
+    helpembed.add_field(name=f"{config['prefix']}restart",
+                        value="Restarts the Terraria server",
+                        inline=False)
+    helpembed.add_field(name=f"{config['prefix']}help",
+                        value="Shows this help menu",
+                        inline=False)
+
+    await interaction.response.send_message(embed=helpembed)
+
 
 # Runs the bot using the bot token
 bot.run(config['token'])
